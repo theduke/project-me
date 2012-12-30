a897c540e93d7973d49c0a489a9886f840c8808b
theduke <chris@theduke.at>
2012-12-30 22:49:00 +0100

    Initial commit. Added blank rails app.

---
+++ project_me/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMe::Application.config.secret_token = '027f9cae62100cc3ce8a154741efabbdaa5c382dbfe4fb2a405cf7f876d3ab0d40bfd1b2538b7441322bf8ea3c9fa4b7060c5c46e7dcf93f3ae23915504d6f67'
